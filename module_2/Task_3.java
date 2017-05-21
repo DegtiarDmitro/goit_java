69c2d90f95302bd60a2e5f5cbbf8d0fb1718f943
DegtiarDmitro <degtiar.dmytro@gmail.com>
2017-05-21 22:48:58 +0300

    third task in second module

---
+++ module_2/Task_3.java
@@ -0,0 +1,53 @@
+package module_2;
+
+/**
+ * Created by dimas on 21.05.17.
+ */
+public class Task_3 {
+
+    /*
+        nput
+
+
+        String ownerName = Ann
+        double withdrawal = 100;
+
+        Output
+        Ann 100 145
+    */
+    public static void main(String[] args) {
+
+        int[] balances = {1200, 250, 2000, 500, 3200};
+        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+        Task_3 bank = new Task_3();
+        bank.withdrawMoney(balances, ownerNames, "Jack", 400);
+
+    }
+
+
+    public void withdrawMoney(int[] balances, String[] ownerNames, String ownerName, double withdrawal){
+
+
+        for(int i = 0; i < ownerNames.length; ++i){
+            if(ownerNames[i].equals(ownerName)){
+
+                int balance = balances[i];
+                double commision = withdrawal * .05;
+
+                if(balance > commision + withdrawal){
+                    System.out.println(ownerName + " " + withdrawal + " " + Math.round(balance - withdrawal - commision));
+                }else {
+                    System.out.println("Oww NO");
+                }
+                break;
+            }
+        }
+
+
+    }
+
+
+
+
+}
