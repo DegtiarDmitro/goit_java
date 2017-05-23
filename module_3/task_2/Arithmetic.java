f6c9e5e24d52b53096274000a29bf488e4d0901a
degtiar.dmytro <degtiar.dmytro@gmail.com>
2017-05-23 16:35:35 +0300

    task 2 module 3

---
+++ module_3/task_2/Arithmetic.java
@@ -0,0 +1,11 @@
+package module_3.task_2;
+
+/**
+ * Created by dima on 5/23/2017.
+ */
+public class Arithmetic {
+
+    public int add(Integer a, Integer b){
+        return a + b;
+    }
+}
---
+++ module_3/task_2/Main.java
@@ -0,0 +1,12 @@
+package module_3.task_2;
+
+/**
+ * Created by dima on 5/23/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Arithmetic arithmetic = new Arithmetic();
+        System.out.println(arithmetic.add(5,7));
+    }
+}
