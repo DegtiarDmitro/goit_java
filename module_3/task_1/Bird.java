0e84aad0aad3c692dbbb9dc53dacc33db8df9238
degtiar.dmytro <degtiar.dmytro@gmail.com>
2017-05-23 16:36:37 +0300

    task 2 module 3

---
+++ module_3/task_1/Bird.java
@@ -0,0 +1,11 @@
+package module_3.task_1;
+
+/**
+ * Created by dima on 5/23/2017.
+ */
+public class Bird {
+
+    public void sing(String song){
+        System.out.println(song);
+    }
+}
--- module_3/task_1/Main.java
+++ module_3/task_1/Main.java
@@ -1,4 +1,5 @@
-package module_3;
+package module_3.task_1;
+
 
 /**
  * Created by dima on 5/23/2017.
