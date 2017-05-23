c397a6e8690cdbd490c5639f473be325e4d76b0e
degtiar.dmytro <degtiar.dmytro@gmail.com>
2017-05-23 16:26:09 +0300

    task 1 module 3

---
+++ module_3/Main.java
@@ -0,0 +1,26 @@
+package module_3;
+
+/**
+ * Created by dima on 5/23/2017.
+ */
+public class Main {
+
+    /*
+     I am walking
+
+ I am flying
+
+ I am singing
+
+ I am Bird
+     */
+    public static void main(String[] args) {
+
+        Bird bird = new Bird();
+        bird.sing("I am walking");
+        bird.sing("I am flying");
+        bird.sing("I am singing");
+        bird.sing("I am Bird");
+
+    }
+}
