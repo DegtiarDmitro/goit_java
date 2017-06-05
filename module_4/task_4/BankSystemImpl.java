e6fe5510e7786efbde7ebefa69390d3039c49b2a
DegtiarDmitro <degtiar.dmytro@gmail.com>
2017-06-05 13:47:51 +0300

    fourth task in fourth module

---
+++ module_4/task_4/BankSystemImpl.java
@@ -0,0 +1,7 @@
+package module_4.task_4;
+
+/**
+ * Created by dimas on 05.06.17.
+ */
+public class BankSystemImpl implements BankSystem {
+}
---
+++ module_4/task_4/User.java
@@ -0,0 +1,30 @@
+package module_4.task_4;
+
+import module_4.task_3.Bank;
+
+/**
+ * Created by dimas on 05.06.17.
+ */
+public class User {
+
+    private long id;
+    private String name;
+    private double balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private Bank bank;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                ", monthsOfEmployment=" + monthsOfEmployment +
+                ", companyName='" + companyName + '\'' +
+                ", salary=" + salary +
+                ", bank=" + bank +
+                '}';
+    }
+}
